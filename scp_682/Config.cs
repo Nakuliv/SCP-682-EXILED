912e1cee3eac544f5d35947807570972a1d6a52f
Cwaniaak <ccwaniaktv@gmail.com>
2021-09-14 00:56:53 +0200

    bruh

--- scp_682/Config.cs
+++ scp_682/Config.cs
@@ -13,10 +13,8 @@ namespace scp_682
         public bool IsEnabled { get; set; } = true;
         public int spawn_chance { get; set; } = 100;
 
-        [Description("The text which gets displayed over the Player Name of Scp682")]
+        [Description("Text to be displayed above player name")]
         public string DisplayName { get; set; } = "SCP-682";
-
-        [Description("The Color which the text of Scp682 should have")]
         public string DisplayColor { get; set; } = "red";
 
         public Exiled.API.Features.Broadcast spawn_message { get; private set; } = new Exiled.API.Features.Broadcast("<b>You are <color=red>SCP-682</color></b>", 10);
@@ -24,22 +22,22 @@ namespace scp_682
         public bool can_kill_on_oneshot { get; set; } = false;
         [Description("whether SCP-682 is to be able to Pry Gates?")]
         public bool can_PryGates { get; set; } = true;
-        [Description("how much hp should SCP-682 get when he damage a human")]
         public int PryGateCooldown { get; set; } = 60;
         public string PryGateCooldownMessage { get; set; } = "You must wait %time% seconds to pry gate again.";
+
+        [Description("how much hp should SCP-682 get when it damage a human")]
         public int heal_hp_when_kill { get; set; } = 5;
         [Description("max hp")]
-        public int MaxHP { get; set; } = 1800;
+        public int MaxHP { get; set; } = 2200;
         [Description("every how many seconds SCP-682 health should regenerate?")]
         public int heal_time { get; set; } = 5;
         [Description("how much hp should be regenerated?")]
         public int heal_hp { get; set; } = 5;
         [Description("can scp 682 destroy doors?")]
         public bool scp682_can_destroy_door { get; set; } = true;
-        [Description("how many % chance should SCP-682 have to destroy the door")]
-
         public int DestroyDoorCooldown { get; set; } = 30;
         public string DestroyDoorCooldownMessage { get; set; } = "You must wait %time% seconds before destroying the door again.";
+        [Description("how many % chance should SCP-682 have to destroy the door")]
         public int scp682_destroy_door_chance { get; set; } = 100;
     }
 }
